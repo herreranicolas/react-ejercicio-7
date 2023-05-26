37bd85d41699313092d81b5564a0749049dab865
Nicolas Herrera <herreranicoo17@gmail.com>
2023-05-25 22:12:01 -0300

    Agrego el componente EmpleadoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Container } from "react-bootstrap";
+import EmpleadoList from "./components/EmpleadoList";
 
 function App() {
-  return <></>;
+  return (
+    <Container>
+      <EmpleadoList />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/EmpleadoList.jsx
@@ -0,0 +1,7 @@
+import { ListGroup } from "react-bootstrap";
+
+function EmpleadoList() {
+  return <ListGroup></ListGroup>;
+}
+
+export default EmpleadoList;
